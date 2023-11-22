ea14d14a1f71751c70e152cc733e49c9e96adceb
Kim Hyeonwoo <1reds@naver.com>
2023-11-22 13:30:22 +0900

    Add zip files

--- Assets/Scripts/Stage.cs
+++ Assets/Scripts/Stage.cs
@@ -3,22 +3,34 @@ using UnityEngine;
 
 public class Stage : MonoBehaviour
 {
-    public Stage(bool ZeroStage, bool Speed, bool bCover)
-    {
-        SpeedUp = Speed;
-        ZeroClear = ZeroStage;
-        Cover = bCover;
-        if (ZeroClear)
-            LeverList.Add(0);
-        if (SpeedUp)
-            LeverList.Add(1);
-        if (Cover)
-            LeverList.Add(2);
-    }
     private bool SpeedUp;
     private bool ZeroClear;
     private bool Cover;
-    private List<int> LeverList = new List<int>();
+    private List<int> LeverList;
+
+    public Stage(bool ZeroStage, bool Speed, bool bCover)
+    {
+        LeverList = new List<int>();
+
+        SpeedUp = Speed;
+        ZeroClear = ZeroStage;
+        Cover = bCover;
+
+        if (ZeroClear)
+        {
+            LeverList.Add(0);
+        }
+
+        if (SpeedUp)
+        {
+            LeverList.Add(1);
+        }
+
+        if (Cover)
+        {
+            LeverList.Add(2);
+        }
+    }
 
     public int SelectLever() // 들어간 레버별 숫자에서 랜덤으로 하나를 고른다.
     {
